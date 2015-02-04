2450569b4ea53aa0bdc4a56bde6acc67a3adf261
ethank <kes0228@gmail.com>
2015-02-04 09:50:46 +0900

    현재 작업중

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Senop - shopping mall</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+<div id="wrap">
+<header>
+	<div id="logo"><img src="imgs/senop_logo.png" alt="Senop" width="176" height="67"> </div>
+    <div id="head_link">
+    	<ul>
+        	<li><a href="#">로그인</a></li>
+            <li><a href="#">회원가입</a></li>
+            <li><a href="#">장바구니</a></li>
+            <li><a href="#">고객센터</a></li>
+            <li class="no_line"><a href="#">주문배송</a></li>
+        </ul>
+    </div>
+	<br>
+    <div id="search">
+    	<form>
+           <input name="input" type="text" class="main_input"> 
+           <input name="search" type="button" value="검 색" class="btn1">
+        </form>
+    </div>
+</header>
+<div class="clear"></div>
+	<aside>
+		<nav>
+			<ul>
+				<li><h3>국내외 도서</h3>
+					<ul>
+						<li><a href="#">컴퓨터와 인터넷</a></li>
+						<li><a href="#">비즈니스와 경제</a></li>
+						<li><a href="#">대학교재</a></li>
+						<li><a href="#">자기관리</a></li>
+						<li><a href="#">자연과 과학</a></li>
+						<li><a href="#">역사와 문화</a></li>
+					</ul>
+				</li>
+				<li><h3>digital 기</h3>
+					<ul>
+						<li><a href="">dslr 및 lens</a></li>
+						<li><a href="">스마트폰</a></li>
+						<li><a href="">노트북</a></li>
+						<li><a href="">pc 본체</a></li>
+						<li><a href="">컴퓨터 부품</a></li>
+						<li><a href="">악세서리 </a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+		<div id="banner">
+			<img src="imgs/banners/nikon1.jpg" width="190" height="163">
+		</div>
+		<div id="notice">
+			<h2>공지사항</h2>
+				<ul>
+					<li><a href="#">여긴 공지사항입니다.</a></li>
+					<li><a href="#">여긴 공지사항입니다.</a></li>
+					<li><a href="#">여긴 공지사항입니다.</a></li>
+					<li><a href="#">여긴 공지사항입니다.</a></li>
+					<li><a href="#">여긴 공지사항입니다.</a></li>		
+				</ul>
+
+		</div>
+		<div id="banner">
+			<img src="imgs/banners/slate.jpg" width="190" height="362">
+		</div>
+	</aside>
+	<article>
+		<div id="ad_main_banner">
+			<img src="imgs/banners/main_banner1.jpg" width="775" height="145" alt="">
+		</div>
+		<div id="bestseller">
+			<h3>베스트 셀러</h3>
+			<div class="book">
+				<img src="imgs/products/book/html5.jpg" width="121" height="154">
+				<div class="sort">[컴퓨터 인터넷]</div>
+				<div class="title">[처음부터 다시배우는 HTML5</div>
+				<div class="writer">양용석 | 로드북</div>
+				<div class="price">25000원</div>
+			</div>
+		</div>
+		<div id="dslr_lens">
+			
+		</div>
+		<div id="ad_sub_banner"></div>
+		<div id="laptop"></div>
+	</article>
+	<footer></footer>
+</div>
+</body>
+</html>
