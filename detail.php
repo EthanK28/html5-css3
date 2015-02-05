f26f111489c1230f37807dc8c189ed17b6155aff
eskang <kes0228@gmail.com>
2015-02-05 14:50:10 +0900

    Detail page done. Working on tab-switching function now

--- detail.php
+++ detail.php
@@ -5,7 +5,147 @@
 		<h1>컴퓨터와 인터넷</h1>
 		<h2>구내외 도서 > 컴퓨터와 인터넷 > 웹 개발</h2>
 		<h3>처음부터 다시 배우는 HTML5 & CSS3 : 실전 웹 표준 사이트 구축 까지		</h3>
+		<h4>양용석 | 로드북	</h4>
 	</hgroup>
+    <div id="goods_image">
+        <figure> <img src="imgs/products/book/HTML5CSS3.jpg" alt="HTML5 &amp; CSS3"></figure>
+    </div>
+    <div id="detail_table">
+    <table>
+        <tr class="dot_line">
+            <td class="fixed">정가</td><td><span>25,000원</span></td>
+        </tr>
+        <tr class="dot_line">
+          <td class="fixed">판매가</td>
+          <td><span class="active">22,500원 (10% 할인)</span></td>
+        </tr>
+        <tr class="dot_line">
+          <td class="fixed">포인트적립</td>
+          <td class="active">2250P(10%적립)</td>
+        </tr>
+        <tr class="dot_line">
+          <td class="fixed">포인트 추가적립</td>
+          <td>만원이상 구매시 1,000P, 5만원이상 구매시 2,000P추가적립 편의점 배송 이용시 300P 추가적립 </td>
+        </tr>    
+        <tr class="dot_line">
+          <td class="fixed">발행일</td>
+          <td>2011년 10월 04일</td>
+        </tr>
+        <tr class="dot_line">
+          <td class="fixed">페이지 수 </td>
+          <td>448쪽</td>
+        </tr>
+        <tr class="dot_line">
+          <td class="fixed">ISBN</td>
+          <td>9788996659839 </td>
+        </tr>
+        <tr class="dot_line">
+          <td class="fixed">배송료</td>
+          <td><strong>무료</strong></td>
+        </tr>
+        <tr class="dot_line">
+          <td class="fixed">배송안내</td>
+          <td><strong>[당일배송]</strong> 당일배송 서비스 시작!<br>
+              <strong>[휴일배송]</strong> 휴일에도 배송받는 Senop</td>
+        </tr>
+        <tr class="dot_line">
+          <td class="fixed">도착예정일</td>
+          <td>지금 주문 시 내일 도착 예정  </td>
+        </tr>
+        <tr class="dot_line">
+          <td class="fixed">수량 </td>
+          <td><select name="" style="width:60px;">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select></td>
+        </tr>
+    </table>
+    <ul>
+        <li><a href="#" class="buy">구매하기 </a></li>
+        <li><a href="#" class="cart">장바구니</a></li>
+        <li><a href="#" class="wish">위시리스트</a></li>
+    </ul>
+    </div>
+    <div class="clear"></div>
+    <!-- 내용 들어 가는 곳 -->
+    <div id="container">        
+        <ul class="tabs">
+            <li><a href="#tab1">책소개</a></li>
+            <li><a href="#tab2">저자소개</a></li>
+            <li><a href="#tab3">책목차</a></li>        
+            <li><a href="#tab4">출판사서평</a></li>
+            <li><a href="#tab5">추천사</a></li>   
+            <li><a href="#tab6">리뷰</a></li>                      
+        </ul>
+        <div class="tab_container">            
+            <div id="tab1" class="tab_content">
+            	<h4>책소개</h4>
+              <p>
+                HTML5와 CSS3의 새로운 점에 포커스를 맞추고 있지는 않다. 웹 표준의 핵심 철학인 "구조와 디자인의 분리"라는 관점에 맞게 달라진 웹 개발 방식을 제대로 배울 수 있도록 하는 것이 이 책의 주목적. 이 책으로 학습하는 독자가 HTML5로 구조를 제대로 설계할 수 있고 CSS3로 정밀하고 자유자재로 디자인을 컨트롤할 수 있는 정도의 수준까지 될 수 있도록 구성하였다. 특히, 3부에서는 최신의 브라우저를 기준으로 HTML5와 CSS3를 이용하여 가상이지만 대표할 만한 기업의 웹 표준 사이트를 구축해본다. 
+                </p>
+            <img src="imgs/products/book/14462107.jpg" width="600" height="771">
+            
+            </div>
+            
+            <div id="tab2" class="tab_content">
+            	<h4>저자소개</h4>
+                <p>
+                <div class="writer">저자 : 양용석</div>
+두산정보통신 인터넷 사업팀에서 첫 직장생활을 시작으로, 두산 그룹, 국제 유도연맹, 두산 베어스 등의 웹사이트를 개발했다. 그 후 다양한 인터넷 회사에서 웹사이트를 개발하였고 지금은 프리랜서로 활동하고 있다. 최근 두바이 아시아나 호텔 사이트를 개발했다. 제주대학교에서 정보공학(현 컴퓨터공학)을 전공했으며, 현재 제주도에서 아내와 두 딸 그리고 아들과 함께 살고 있다. 
+
+                
+                
+                </p>
+
+            </div>
+            
+            <div id="tab3" class="tab_content">
+            	<h4>책목차</h4>
+                <p>                
+                    1부 웹 표준과 HTML & CSS의 기초<br><br>
+                    
+                    01장 웹 표준이란 <br>
+                    Section 1 웹 표준의 등장 배경 <br>
+                    Section 2 웹 접근성을 높여주는 웹 표준 <br>
+                    Section 3 환경을 보호하는 웹 표준 <br>
+                    Section 4 사이트 개발 기간의 단축과 유지 보수의 용이성 <br>
+                    Section 5 정밀하게 제어할 수 있는 사이트 디자인 <br><br>
+                    
+                    
+                    02장 HTML5 소개 <br>
+                    Section 1 DOCTYPE 정의하기 <br>
+                    Section 2 HTML 문서의 기본 구조 <br>
+                    Section 3 HTML5와 XHTML1.0의 차이점 <br>
+                    Section 4 HTML5에서 추가된 태그 <br>
+                    Section 5 HTML5에서 사라진 태그 <br><br>
+                    
+                    
+                    03장 웹사이트 구조 설계를 통해 살펴 본 HTML5의 장점<br>
+                    Section 1 HTML 문서의 구조 설계 <br>
+                    Section 2 HTML5를 이용한 HTML 문서 구조 잡기 <br><br>
+
+                </p>
+
+            </div>
+            
+                 
+            <div id="tab4" class="tab_content">
+            	<h4>출판사서평</h4>
+
+            </div>
+            <div id="tab5" class="tab_content">
+            	<h4>추천사</h4>
+
+            </div>    
+            <div id="tab6" class="tab_content">
+            	<h4>리뷰</h4>
+ 
+            </div>
+         </div>   
+    </div>
 
 </article>
 
