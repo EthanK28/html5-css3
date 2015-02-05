f1bd44483db830d52473c9e13d4f41171252efc7
ethank <kes0228@gmail.com>
2015-02-05 10:16:57 +0900

    제품 리스트부분 실습

---
+++ list.php
@@ -0,0 +1,72 @@
+<?php include "header.php" ?>
+<?php include "side.php" ?>
+<article>
+	<hgroup>
+		<h1>컴퓨터와 인터넷</h1>
+		<h2>국내와 도서 > 컴퓨터와 인터넷 > 웹개</h2>
+	</hgroup>
+	<section id="new_book"> <!-- 섹션을 이용 전체적인 레이아웃을 잡아줌 -->
+		<h3>새로 나온 책</h3> <!-- 새로 나온 책 제목 처리, 여기가 1번 영역 -->
+		<div id='left_scroll'><a href='javascript:slide("left");'><img src="imgs/left.gif" alt=""></a>
+		<div id="carousel_inner">
+			<ul id="carousel_ul">
+				<li>
+					<div id="book"> <!-- 책 내용 부분, 여기는 -->
+						<a href=""><img src="imgs/products/book/html5.jpg" width="100" alt=""></a>
+						<div class="sort">[컴퓨터와 인터넷]</div>
+						<div class="title"></div>
+						<div class="writer">양용석</div>
+						<div class="price"><span>25,000원</span><br>22,500원</div>
+					</div>
+				</li>
+				<li>
+					<div id="book"> <!-- 책 내용 부분, 여기는 -->
+						<a href=""><img src="imgs/products/book/html5.jpg" width="100" alt=""></a>
+						<div class="sort">[컴퓨터와 인터넷]</div>
+						<div class="title"></div>
+						<div class="writer">양용석</div>
+						<div class="price"><span>25,000원</span><br>22,500원</div>
+					</div>
+				</li>			
+				<li>
+					<div id="book"> <!-- 책 내용 부분, 여기는 -->
+						<a href=""><img src="imgs/products/book/html5.jpg" width="100" alt=""></a>
+						<div class="sort">[컴퓨터와 인터넷]</div>
+						<div class="title"></div>
+						<div class="writer">양용석</div>
+						<div class="price"><span>25,000원</span><br>22,500원</div>
+					</div>
+				</li>
+				<li>
+					<div id="book"> <!-- 책 내용 부분, 여기는 -->
+						<a href=""><img src="imgs/products/book/html5.jpg" width="100" alt=""></a>
+						<div class="sort">[컴퓨터와 인터넷]</div>
+						<div class="title"></div>
+						<div class="writer">양용석</div>
+						<div class="price"><span>25,000원</span><br>22,500원</div>
+					</div>
+				</li>
+				<li>
+					<div id="book"> <!-- 책 내용 부분, 여기는 -->
+						<a href=""><img src="imgs/products/book/html5.jpg" width="100" alt=""></a>
+						<div class="sort">[컴퓨터와 인터넷]</div>
+						<div class="title"></div>
+						<div class="writer">양용석</div>
+						<div class="price"><span>25,000원</span><br>22,500원</div>
+					</div>
+				</li>		
+				<li>
+					<div id="book"> <!-- 책 내용 부분, 여기는 -->
+						<a href=""><img src="imgs/products/book/html5.jpg" width="100" alt=""></a>
+						<div class="sort">[컴퓨터와 인터넷]</div>
+						<div class="title"></div>
+						<div class="writer">양용석</div>
+						<div class="price"><span>25,000원</span><br>22,500원</div>
+					</div>
+				</li>									
+			</ul>
+		</div>
+		<div id='right_scroll'><a href='javascript:slide("right");'><img src="imgs/right.gif" alt=""></a>
+	</section>
+</article>
+<?php include "footer.php" ?>
